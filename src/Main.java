e209495ff44cb3d1a2b7b1f49ac870545d0cdf9d
PustovayaTS <ais_ta@bk.ru>
2022-11-09 13:07:41 +0700

    Исправлено задание № 2 блок 3. Введение в коллекции. Хеш и хеш-функцию Множества

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        ArrayList <String> taskList = new ArrayList<>();
-        int j = 2;
-        for (int i = 2; i <= 9; i++) {
-            while (j <= 9){
-                taskList.add(i + "*" + j);
-                j++;
-            }
-            j = i+1;
-        }
+        TaskList taskList = new TaskList();
         System.out.println(taskList);
-        TaskList taskList1 = new TaskList();
-        Random r = new Random();
-        int x = r.nextInt(taskList.size());
-        while (taskList1.getSize()<15){
-            taskList1.addList(taskList.get(x));
-            x = r.nextInt(taskList.size());
-        }
-        System.out.println(taskList1);
     }
 }
--- src/TaskList.java
+++ src/TaskList.java
@@ -1,17 +1,24 @@
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 public class TaskList {
-    private Set<String> setList = new HashSet<>();
+    private final Set<Task> setList = new HashSet<>();
 
-    public void addList(String str){
-        setList.add(str);
+    public TaskList() {
+        Random random = new Random();
+        while (setList.size() < 15) {
+            setList.add(new Task(random.nextInt((9-2) + 1) + 2,
+                    random.nextInt((9-2) + 1) + 2));
+        }
     }
-
-    public int getSize(){return setList.size();}
 
     @Override
     public String toString() {
-        return "Реши примеры:" + setList;
+        String str = new String();
+        for (Task task : setList) {
+            str = str + task.toString();
+        }
+        return str;
     }
 }
